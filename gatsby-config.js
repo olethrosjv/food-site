507146831bc8a4aedf6c9b0c94c90ed2cb1cf333
Joe Vaughn <31394713+olethrosjv@users.noreply.github.com>
2019-09-10 15:50:03 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,8 +36,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `gatsby-restaurant-starter`,
+        short_name: `food-site`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
