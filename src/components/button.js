baf4b95ac4524efa0044982821b12a5c72abe773
olethrosjv <chefjoevaughn@gmail.com>
2019-09-21 07:18:29 -0700

    reservation button

--- src/components/button.js
+++ src/components/button.js
@@ -3,10 +3,14 @@ import { Link } from "gatsby"
 
 const Button = () => (
     <div style={{ margin: '2em, 0, 2em, 0', padding: '2em, 0, 2em, 0'}}>
-        <button type="button" onclick="">
+        <button id="myButton" type="button" onclick="">
+           <Link to="./form-page" style={{ textDecoration: 'none', color: 'black' }}>
            Reserve a Table
+           </Link>
         </button>    
     </div>
 )
+
+
 
 export default Button
